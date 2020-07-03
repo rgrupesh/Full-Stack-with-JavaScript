4e0d7576b3ebf094acec93d788c1ae3b0923b250
Rupesh Gelal <rupeshgelal9@gmail.com>
2020-07-03 18:45:53 +0545

    mainComponent

--- React/my-app/src/components/MenuComponents.js
+++ React/my-app/src/components/MenuComponents.js
@@ -1,49 +1,16 @@
 import React, { Component } from "react";
-import { Card, CardImg, CardImgOverlay, CardBody,CardText, CardTitle } from "reactstrap";
+import { Card, CardImg, CardImgOverlay, CardTitle } from "reactstrap";
+
 
 
 class Menu extends Component{
-
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            selectedDish: null
-
-        }
-        
-    }
-
-    onDishSelcet(dish){
-        this.setState({ selectedDish: dish})
-    }
-
-    renderDish(dish){
-        if (dish != null){
-            return(
-                <Card>
-                <CardImg width="100%" src={dish.image} alt={dish.name} />
-                <CardBody>
-                    <CardTitle>{dish.name}</CardTitle>
-                    <CardText>{dish.description}</CardText>
-                </CardBody>
-                </Card>
-
-            );    
-        }
-        else {
-            return(
-                <div></div>
-            );
-        }
-    }
 
     render(){
 
         const menu= this.props.dishes.map((dish) => {
             return (
-                <div key={dish.id} className="col-12 col-md-5 m-1">
-                    <Card onClick={() => this.onDishSelcet(dish)}>
+                <div Cardkey={dish.id} className="col-12 col-md-5 m-1">
+                    <Card onClick={() => this.props.onClick(dish.id)}>
                         <CardImg width="100%" src={dish.image} alt={dish.name} />
                         <CardImgOverlay>
                             <CardTitle>{dish.name}</CardTitle>
@@ -57,9 +24,6 @@ class Menu extends Component{
             <div className="container">
                 <div className="row">
                     {menu}
-                </div>
-                <div className="row">
-                    {this.renderDish(this.state.selectedDish)}
                 </div>
             </div>
 
