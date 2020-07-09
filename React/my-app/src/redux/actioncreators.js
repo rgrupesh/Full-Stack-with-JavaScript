93dbab40aa9dc4913badb3d5447e277d854ae9f3
Rupesh Gelal <rupeshgelal9@gmail.com>
2020-07-09 17:45:38 +0545

    redux thunk

--- React/my-app/src/redux/actioncreators.js
+++ React/my-app/src/redux/actioncreators.js
@@ -1,4 +1,6 @@
 import * as actiontypes from "./actiontypes";
+import { DISHES } from '../shared/dishes';
+
 
 export const addComment = (dishId, rating, author, comment) => ({
     type: actiontypes.ADD_COMMENT,
@@ -8,4 +10,27 @@ export const addComment = (dishId, rating, author, comment) => ({
         author: author,
         comment: comment
     }
+});
+
+export const fetchDishes = () => (dispatch) => {
+
+    dispatch(dishesLoading(true));
+
+    setTimeout(() => {
+        dispatch(addDishes(DISHES));
+    }, 2000);
+}
+
+export const dishesLoading = () => ({
+    type: actiontypes.DISHES_LOADING
+});
+
+export const dishesFailed = (errmess) => ({
+    type: actiontypes.DISHES_FAILED,
+    payload: errmess
+});
+
+export const addDishes = (dishes) => ({
+    type: actiontypes.ADD_DISHES,
+    payload: dishes
 });
--- React/my-app/src/redux/dishes.js
+++ React/my-app/src/redux/dishes.js
@@ -1,10 +1,21 @@
-import { DISHES } from '../shared/dishes';
+import * as actiontypes from "./actiontypes";
 
 
-export const Dishes = (state=DISHES, action) =>{
-    switch(action.type){
+export const Dishes = (state = { isLoading: true,
+    errMess: null,
+    dishes:[]}, action) => {
+    switch (action.type) {
+        case actiontypes.ADD_DISHES:
+            return {...state, isLoading: false, errMess: null, dishes: action.payload};
+
+        case  actiontypes.DISHES_LOADING:
+            return {...state, isLoading: true, errMess: null, dishes: []}
+
+        case  actiontypes.DISHES_FAILED:
+            return {...state, isLoading: false, errMess: action.payload};
+
         default:
             return state;
-            
     }
-}
+};
+
