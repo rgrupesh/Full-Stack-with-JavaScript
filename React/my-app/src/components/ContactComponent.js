51681574db7c57a085b319b4dad193848e7fd04f
Rupesh Gelal <rupeshgelal9@gmail.com>
2020-07-09 18:42:29 +0545

    forms in react-redux

--- React/my-app/src/components/ContactComponent.js
+++ React/my-app/src/components/ContactComponent.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { Button, Label, Col, Row} from "reactstrap";
-import { Control, LocalForm, Errors } from "react-redux-form";
+import { Control, Form, Errors , actions} from "react-redux-form";
 
 const required = (val) => val && val.length;
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
@@ -21,6 +21,8 @@ class Contact extends Component {
     handleSubmit(values){
         console.log("Current state is : " + JSON.stringify(values));
         alert("Current state is : " + JSON.stringify(values));
+        this.props.resetFeedbackForm();
+
     }
 
     render(){
@@ -58,7 +60,7 @@ class Contact extends Component {
                             <h3> send your feedback</h3>
                         </div>
                         <div className="col-12 col-md-9">
-                            <LocalForm onSubmit={(values) => this.handleSubmit(values)}>
+                            <Form model="feedback" onSubmit={(values) => this.handleSubmit(values)}>
                                 <Row className="form-group">
                                     <Label htmlFor="firstname" md={2}>First Name</Label>
                                     <Col md={10}>
@@ -181,7 +183,7 @@ class Contact extends Component {
                                     </Col>
 
                                 </Row>
-                            </LocalForm>
+                            </Form>
                         </div>
                     </div>
             </div>
---
+++ React/my-app/src/redux/forms.js
@@ -0,0 +1,9 @@
+export const InitialFeedback ={
+    firstname:"",
+    lastname:"",
+    telno:"",
+    email:"",
+    agree: "",
+    contactType:"",
+    message:""
+}
