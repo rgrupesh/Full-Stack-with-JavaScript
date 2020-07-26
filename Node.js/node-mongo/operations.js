a91ab220a5ada2d0b695269d91edb5db45a56a8c
Rupesh Gelal <rupeshgelal9@gmail.com>
2020-07-26 14:49:42 +0545

    Promises

--- Node.js/node-mongo/operations.js
+++ Node.js/node-mongo/operations.js
@@ -4,41 +4,24 @@ const assert = require("assert");
 exports.insertDocument = (db , document, collection,callback) =>{
 
     const coll = db.collection(collection);
-    coll.insert(document, (err, result) => {
-        assert.equal(err, null);
-        console.log("Inserted : \n" + result.result.n + "documents into the collection" + collection );
-        callback(result);
-
-    });
+    return coll.insert(document);
 
 };
 
 exports.findDocuments = (db ,collection,callback) =>{
     const coll = db.collection(collection);
-    coll.find({}).toArray((err, docs) =>{
-        assert.equal(err, null);
-        callback(docs);
-    });
+    return coll.find({}).toArray();
     
 };
 
 exports.removeDocuments = (db ,document, collection,callback) =>{
     const coll = db.collection(collection);
-    coll.deleteOne(document, (err, result) => {
-        assert.equal(err, null);
-        console.log("Removed the documents", document);
-        callback(result);
-
-    });
+    return coll.deleteOne(document);
     
 };
 
 exports.updateDocument = (db ,document, update, collection,callback) =>{
     const coll = db.collection(collection);
-    coll.updateOne(document, { $set : update }, null, (err, result) => {
-        assert.equal(err, null);
-        console.log("Updated the documents with ", update);
-        callback(result);
-    });
+    return coll.updateOne(document, { $set : update }, null);
 };
 
