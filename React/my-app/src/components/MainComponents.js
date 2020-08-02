c3fefc76ea2b97ef277e064104ba2f09c5b7a568
Rupesh Gelal <rupeshgelal9@gmail.com>
2020-08-02 10:11:27 +0545

    minor fix

--- React/my-app/src/components/MainComponents.js
+++ React/my-app/src/components/MainComponents.js
@@ -85,7 +85,7 @@ class Main extends Component {
         <TransitionGroup>
           <CSSTransition key={this.props.location.key} classNames="page" timeout={300}>
           <Switch>
-            <Route path="/home" component={HomePage} />
+            
             <Route exact path="/menu" component={() => <Menu dishes= {this.props.dishes} />} />
             <Route path="/menu/:dishId" component={DishWithId} />
             <Route exact path='/contactus' component={() => <Contact resetFeedbackForm={this.props.resetFeedbackForm} />} />
